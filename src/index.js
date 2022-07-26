d3ab838e023fca9b5cbcb630215301a6cd529db9
Helgen97 <drmegman@gmail.com>
2022-07-26 16:48:04 +0300

    Added media queries for mobile devices,

--- src/index.js
+++ src/index.js
@@ -1,19 +1,46 @@
+document.addEventListener("touchstart", function () { }, true);
+
 $(document).ready(function () {
-    $("#menu").on("click", "a", function (event) {
-        event.preventDefault();
-        var id = $(this).attr('href'),
-            top = $(id).offset().top;
-        $('body,html').animate({ scrollTop: top }, 1000);
-    });
+  $("#menu").on("click", "a", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1000);
+  });
 });
+
+
+$(document).mouseup(function (e) {
+  const menu = $("#menu");
+  const box = $(".contacts-box");
+
+  if (menu.has(e.target).length === 0) {
+    menu.removeClass('show');
+  }
+  if (box.has(e.target).length === 0) {
+    box.removeClass('ride');
+  }
+
+  if (e.target.id === "arrow") {
+    if(box.hasClass('ride')) {
+      box.removeClass("ride");
+    } 
+    box.addClass("ride")
+  }
+
+  if (e.target.id === "mobile") {
+    menu.addClass("show")
+  }
+});
+
 
 $("#download").on('click', () => {
-    window.open("https://google.com", "_blank");
+  window.open("./files/cv.pdf", "_blank");
 });
 
-var btn = $('#top');
+const btn = $('#top');
 
-$(window).scroll(function() {
+$(window).scroll(function () {
   if ($(window).scrollTop() > 300) {
     btn.show(300);
   } else {
@@ -21,7 +48,7 @@ $(window).scroll(function() {
   }
 });
 
-btn.on('click', function(e) {
+btn.on('click', function (e) {
   e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '500');
+  $('html, body').animate({ scrollTop: 0 }, '500');
 });
