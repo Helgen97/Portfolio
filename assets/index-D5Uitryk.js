5c265ef50cf282ef181f09aa75977e07e89b2cb4
Helgen97 <drmegman@gmail.com>
2025-09-24 15:47:03 +0300

    Updates

--- assets/index-D5Uitryk.js
+++
@@ -1,2 +0,0 @@
-import{r as t,j as o,A as n}from"./index-DmsxMVUr.js";const r=()=>o.jsx("div",{className:"about_block",role:"article","aria-label":"About me content",children:n.map((e,a)=>o.jsx("p",{className:"paragraph_block",children:e},a))}),s=t.memo(r),c=()=>o.jsx("div",{className:"content",role:"region","aria-label":"About section",children:o.jsx(s,{})}),i=t.memo(c);export{i as default};
-//# sourceMappingURL=index-D5Uitryk.js.map
